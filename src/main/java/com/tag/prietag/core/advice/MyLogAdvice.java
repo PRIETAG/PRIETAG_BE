420fa3e2ec43ec4a6966724db79be40e8334d0e8
Lee-yh2 <dbsgud101@naver.com>
2023-06-16 00:22:21 +0900

    conf: deploy설정 변경

--- src/main/java/com/tag/prietag/core/advice/MyLogAdvice.java
+++ src/main/java/com/tag/prietag/core/advice/MyLogAdvice.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -15,15 +16,18 @@ import java.lang.reflect.Method;
 @Aspect
 @Component
 public class MyLogAdvice {
-    @Pointcut("@annotation(shop.mtcoding.metablog.core.annotation.MyLog)")
-    public void myLog(){}
+    @Pointcut("@annotation(com.tag.prietag.core.annotation.MyErrorLog)")
+    public void myErrorLog(){}
 
-    // After : 정상, 실패 모두 로그 남김
-    // AfterReturning : 정상일때만 로그 남김
-    @AfterReturning("myLog()")
-    public void logAdvice(JoinPoint jp) throws HttpMessageNotReadableException {
-        MethodSignature signature = (MethodSignature) jp.getSignature();
-        Method method = signature.getMethod();
-        log.debug("디버그 : "+method.getName()+" 성공");
+    @Before("myErrorLog()")
+    public void errorLogAdvice(JoinPoint jp) throws Exception {
+        Object[] args = jp.getArgs();
+
+        for (Object arg : args) {
+            if(arg instanceof Exception){
+                Exception e = (Exception) arg;
+                log.error("에러 : "+e.getMessage());
+            }
+        }
     }
 }
